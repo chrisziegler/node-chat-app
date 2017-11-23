a637bf3fe26f9b307e167fa92d40501a46366092
chrisziegler <chrisrziegler@gmail.com>
2017-11-22 21:38:09 -0800

    Added socket.io built-in events for connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
-
-const port = process.env.PORT || 3000;
-// console.log(process.env.PORT);
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
 const app = express();
+var server = http.createServer(app);
+var io = socketIO(server)
 
 app.use(express.static(publicPath));
-// app.get('/', (req, res) => {
-//     res.render('index.html')
-// })
 
 
-app.listen(port, () => {
+// allows us to register an event listener
+// connection -the most populat built-in event
+//socket argument is similiar to the one we use in index.html
+// represents the individual socket, as opposed to all the users connected to the server
+io.on('connection', (socket) => {
+    console.log('New user connected');
+});
+
+server.listen(port, () => {
     console.log(`Server is running on PORT ${port}`)
 })
