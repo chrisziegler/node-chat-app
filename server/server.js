00b754758d37049ff151fa170bb7804d4cbd373c
chrisziegler <chrisrziegler@gmail.com>
2017-11-28 23:31:42 -0800

    added custom event handler demonstrating e-mail functionality, eslintrc

--- server/server.js
+++ server/server.js
@@ -7,12 +7,19 @@ const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 const app = express();
 var server = http.createServer(app);
-var io = socketIO(server)
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newEmail', {
+        from: 'mike@example.com',
+        text: 'Hey. What is going on?',
+        createdAt: 123
+    });
+
     socket.on('disconnect', () => {
         console.log('Client disconnected from server');
     });
