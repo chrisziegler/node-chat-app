f4f952d32e935a14d810cf1cd2d858455dff367d
chrisziegler <chrisrziegler@gmail.com>
2017-12-04 22:06:54 -0800

    added jquery and forms to client

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,27 @@
-
+/* eslint-disable */
 const socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
-});
-
-// like 'connect' a built-in event listener
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.emit('createMessage', {
-  from: 'johndoe@example.com',
-  text: 'Instant messaging is awesome!'
-  // this callback function will fire when
-  // acknowledgement arrives back here at client
-}, function (data) {
-  console.log('data:', data);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  const li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  $('#messages').append(li)
+});
+
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: $('[name=message]').val()
+  }, function () {
+
+  });
 });
